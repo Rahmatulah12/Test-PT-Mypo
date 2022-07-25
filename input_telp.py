b2b6d3946f5b9902509ce5b1e5f4214357f995fc
Rahmatulah Sidik <blcak.gun123@gmail.com>
2022-07-25 21:48:02 +0700

    bug fix logic input telp

--- input_telp.py
+++ input_telp.py
@@ -4,7 +4,7 @@ def input_telp():
     if len(telp) > 20:
         print("Nomor telepon terlalu panjang")
         exit()
-    new_telp = format_telp(telp)
+    new_telp = format_telp2(telp)
     print(new_telp)
 
 def format_telp(input_telp):
@@ -19,6 +19,23 @@ def format_telp(input_telp):
             new_telp += "-"       
     
     return new_telp.rstrip("-")
+
+def format_telp2(input_telp) :
+    digits = ""
+    blk = ""
+    for i in input_telp:
+        if i.isnumeric():
+            blk += i
+        if len(blk) == 3:
+            digits += blk+"-"
+            blk = ""
+
+    if len(blk) == 0:
+        return digits[:-1]
+    elif len(blk) == 1:
+        return digits[:-2]+"-"+digits[-2]+blk
+    elif len(blk) == 2:
+        return digits+blk
     
 if __name__ == "__main__" :
     input_telp()
